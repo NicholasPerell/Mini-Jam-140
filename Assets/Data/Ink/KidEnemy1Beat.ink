c335dfb4c3eb9077e94a4c5b251e8fd5c9341c5c
PlayfreeLabs <pastoraaron0430@gmail.com>
2023-09-01 14:14:28 -0400

    Movement and Start of Main Menu
    added some player controller and background standin for menu

---
+++ Assets/Data/Ink/KidEnemy1Beat.ink
@@ -0,0 +1,4 @@
+// After first enemy is defeated
+Maybe you monsters aren't so tough after all.
+
+    -> END
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,6 +15,11 @@ public class PlayerController : MonoBehaviour
     bool isPlayerTurn = false;
 
     public event UnityAction OnTurnComplete;
+    public LayerMask whatStopsMovement;
+    public float moveSpeed = 2f;
+    public Transform movePoint;
+    public GameObject objectToSpawnvert;
+    public GameObject objectToSpawnhorz;
 
     public void SetFacing(DirectionFacing facing)
     {
@@ -25,6 +30,50 @@ public class PlayerController : MonoBehaviour
     {
         currentLevelData = levelData;
         isPlayerTurn = true;
+    }
+
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05)
+        {
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .35f, whatStopsMovement))
+                {
+                        if (Input.GetKey(KeyCode.D))
+                        {
+                            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                            Instantiate(objectToSpawnhorz, transform.position, objectToSpawnhorz.transform.rotation);
+                        }
+                        if (Input.GetKey(KeyCode.A))
+                        {
+                            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                            Instantiate(objectToSpawnhorz, transform.position, objectToSpawnhorz.transform.rotation);
+                        }
+                }
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .35f, whatStopsMovement))
+                {
+
+                        if (Input.GetKey(KeyCode.W))
+                        {
+                            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                            Instantiate(objectToSpawnvert, transform.position, objectToSpawnvert.transform.rotation);
+                        }
+                        if (Input.GetKey(KeyCode.S))
+                        {
+                            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                            Instantiate(objectToSpawnvert, transform.position, objectToSpawnvert.transform.rotation);
+                        }
+                }
+            }
+        }
     }
 
     private void DeclareTurnOver()
