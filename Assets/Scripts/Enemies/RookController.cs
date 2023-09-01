d3747aa5cc1752ea625faea17aa0518d55a4829e
Nicholas Perell <nicholasperell@gmail.com>
2023-09-01 13:53:06 -0400

    Updated placeholder definitions in RookController

--- Assets/Scripts/Enemies/RookController.cs
+++ Assets/Scripts/Enemies/RookController.cs
@@ -6,11 +6,12 @@ public class RookController : EnemyController
 {
     protected override Vector2Int[] GetAttackVisionTiles(Vector2Int enemy, Vector2Int[] walls)
     {
-        enemy.
+        return new Vector2Int[] { enemy };
     }
 
     protected override Vector2Int[] GetPeripheralVisionTiles(Vector2Int enemy, Vector2Int[] walls)
     {
-        throw new System.NotImplementedException();
+        return new Vector2Int[] { enemy };
+        //throw new System.NotImplementedException();
     }
 }
